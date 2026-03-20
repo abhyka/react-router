4718aead7829cb40589b79439bce34da757bc00a
Halim Yalçınkaya <abh.yalcinkaya@gmail.com>
2026-03-20 10:52:57 +0100

    changed target

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -30,7 +30,6 @@ export function Welcome() {
                   <a
                     className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
                     href={href}
-                    target="_blank"
                     rel="noreferrer"
                   >
                     {icon}
