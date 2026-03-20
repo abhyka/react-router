dafe7b4e76a36fd44515e88495375bd157c76dd2
elitsakutsarova <elitsa.biserova.kutsarova@student.howest.be>
2026-03-20 11:23:26 +0100

    Work in progress

--- app/components/MealCard.tsx
+++ app/components/MealCard.tsx
@@ -1,9 +1,29 @@
 import { useFetcher } from "react-router";
 import "./mealcard.css";
 
+/* export const clientAction = async({params, request}) => {
+  const formData = await request.formData();
+  const catImg = formData.get("imageId");
+  const updates = Object.fromEntries(formData);
+  await addToFavorites(params.imageId, updates);
+  return { favorites: formData };
+}
+ */
+
 const MealCard = ({ meal }) => {
   const fetcher = useFetcher();
   const isSubmitting = Boolean(fetcher.formData);
+
+/*   const fetcher = useFetcher();
+  const { favorites } = useRouteLoaderData("sidebar");
+
+  const favored = favorites.some(
+    (fav) => fav.image && fav.image.id === cat.id
+  );
+
+  const submitted = Boolean(fetcher.formData);
+  const liked = favored || submitted;
+ */
 
   return (
     <div className="meal-card card">
